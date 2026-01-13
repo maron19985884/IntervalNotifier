79b5f6891ede08e4e9fa19bdc6647533d4c3e7b8
maron19985884 <maron5884@icloud.com>
2026-01-14 00:56:43 +0900

    Initial Commit

---
+++ IntervalNotifier/IntervalNotifierApp.swift
@@ -0,0 +1,17 @@
+//
+//  IntervalNotifierApp.swift
+//  IntervalNotifier
+//
+//  Created by Uru on 2026/01/14.
+//
+
+import SwiftUI
+
+@main
+struct IntervalNotifierApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
